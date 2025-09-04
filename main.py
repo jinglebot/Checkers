bfb7ad9113c255fad62b1c65615bf9c97232d406
mariacristina-an <maa7575@g.harvard.edu>
2025-09-03 22:12:34 -0700

    Update make_king, move functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from checkers.constants import WIDTH, HEIGHT
+from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE
 from checkers.board import Board
 import time
 
@@ -7,6 +7,12 @@ FPS = 60
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 def main():
     run = True
@@ -19,8 +25,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 
-            if event.type == pygame.QUIT:
-                pass
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                piece = board.get_piece(row, col)
+                board.move(piece, 4, 3)
             
         board.draw(WIN)
         pygame.display.update()
